83b7e45996e8462ff45db5809c92589a9e451a1f
MB Crosier <mbcrosier@gmail.com>
2024-10-27 21:09:27 -0400

    update vercel analytics for astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import preact from "@astrojs/preact";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), mdx()]
+  integrations: [preact(), mdx()],
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
